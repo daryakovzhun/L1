607d493182273fb2ce3cd7082686a00efb48867d
Rosana Bright <rosanabr@at-f1.msk.21-school.ru>
2023-01-29 19:01:25 +0300

    fix 22

--- 22/main.go
+++ 22/main.go
@@ -21,9 +21,9 @@ func main() {
 	b := new(big.Int)      //  создаем переменную типа big.Int для второго числа
 	b.SetString(b_str, 10) //  задаем значение переменной, считанное из консоли
 
-	sum, sub, mul, div := new(big.Int), new(big.Int), new(big.Int), new(big.Int) //  создаем переменные для операций сложения, вычитания, умножения и деления
-	fmt.Println("Sum:", a, "+", b, "=", sum.Add(a, b))                           //  выводим результат сложения (a+b)
-	fmt.Println("Sub:", a, "-", b, "=", sub.Sub(a, b))                           //  выводим результат вычитания (a-b)
-	fmt.Println("Mul:", a, "*", b, "=", mul.Mul(a, b))                           //  выводим результат умножения (a*b)
-	fmt.Println("Div:", a, "/", b, "=", div.Div(a, b))                           //  выводим результат деления (a/b)
+	sum, sub, mul, div := new(big.Int), new(big.Int), new(big.Int), new(big.Float)                   //  создаем переменные для операций сложения, вычитания, умножения и деления
+	fmt.Println("Sum:", a, "+", b, "=", sum.Add(a, b))                                               //  выводим результат сложения (a+b)
+	fmt.Println("Sub:", a, "-", b, "=", sub.Sub(a, b))                                               //  выводим результат вычитания (a-b)
+	fmt.Println("Mul:", a, "*", b, "=", mul.Mul(a, b))                                               //  выводим результат умножения (a*b)
+	fmt.Println("Div:", a, "/", b, "=", div.Quo(new(big.Float).SetInt(a), new(big.Float).SetInt(b))) //  выводим результат деления (a/b)
 }
