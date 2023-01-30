f85f1c8cd346e78771ce0a9c2e4ea805456765d4
Daria Kovzhun <dariakovzhun@gmail.com>
2023-01-30 12:10:03 +0300

    add method in 1

--- 1/main_1.go
+++ 1/main_1.go
@@ -20,12 +20,22 @@ func (h *Human) SayAge() {
 	fmt.Println(h.age) // печатаем возраст
 }
 
+func (h *Human) SayName() {
+	fmt.Println(h.name) // печатаем имя
+}
+
 type Action struct {
 	Human // наследуемся от Human, также можно (*Human) и (h Human)
+	name  string
+}
+
+func (a *Action) SayName() {
+	fmt.Println(a.name) // печатаем имя
 }
 
 func main() {
-	act := Action{Human{"Ivan", "Ivanov", 22}} // создаем структуру Action, поле которого является структура Human
-	act.SayFullName()                          // вызов метода, который печатаем полное имя
-	act.SayAge()                               // вызов метода, который печатаем возраст
+	act := Action{Human{"Ivan", "Ivanov", 22}, "act"} // создаем структуру Action, поле которого является структура Human
+	act.SayFullName()                                 // вызов метода у структуры Human, который печатаем полное имя
+	act.SayAge()                                      // вызов метода у структуры Human, который печатаем возраст
+	act.SayName()                                     // вызов метода у структуры Action
 }
